6bafab2b991e6eec2779499b26b9f7bb4c5615ae
shirleyzj16 <70995515+shirleyzj16@users.noreply.github.com>
2022-05-21 12:02:36 -0600

    pagechanges

--- page-arte-y-arquitectura.php
+++ page-arte-y-arquitectura.php
@@ -1,25 +1,113 @@
 <?php get_header(); ?>
 
-<h1>Arte y arquitectura </h1>
+<h1>Arte y arquitectura</h1>
+<hr class="division">
 
-<div  class="container-fluid h-75">
-    <div >
-        <img src="images/fototeatro.jpg" alt="">
-    </div>    
-    <div class="info_lugar">
-        <h2>Nombre lugar</h1>
-        <h3>Provincia</h3>
-                <p>
-                El Teatro Nacional de Costa Rica es el principal teatro de Costa Rica.Se le considera uno de los inmuebles más 
+<div class="card mb-3" style="max-width: 1000px;">
+  <div class="row g-5">
+    <div class="col-md-4" >
+      
+    </div>
+    <div class="col-md-8"  >
+      <div class="card-body">
+        <h5 class="card-title">Nombre Lugar</h5>
+        
+        <h6>Ubicacion Lugar</h6>
+        <hr class="line";>
+        <p class="card-text"> El Teatro Nacional de Costa Rica es el principal teatro de Costa Rica.Se le considera uno de los inmuebles más 
                 importantes de la historia nacional, y principal joya arquitectónica de la ciudad de San José. Su construcción 
                 guarda un profundo significado dado que representa la decisión del costarricense a la hora de emprender acción y 
-                de la estabilidad económica (traída por la cosecha de café) y política de la época en la que fue construido.
-                 
-               </p>
+                de la estabilidad económica (traída por la cosecha de café) y política de la época en la que fue construido.</p>
+        
+      </div>
     </div>
-
-
+  </div>
 </div>
+
+<div class="card mb-3" style="max-width: 1000px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Nombre Lugar</h5>
+        <h6>Ubicacion Lugar</h6>
+        <hr class="line";>
+        <p class="card-text"> El Teatro Nacional de Costa Rica es el principal teatro de Costa Rica.Se le considera uno de los inmuebles más 
+                importantes de la historia nacional, y principal joya arquitectónica de la ciudad de San José. Su construcción 
+                guarda un profundo significado dado que representa la decisión del costarricense a la hora de emprender acción y 
+                de la estabilidad económica (traída por la cosecha de café) y política de la época en la que fue construido.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="card mb-3" style="max-width: 1000px;">
+  <div class="row g-0">
+    <div class="col-md-4" class="img-fluid rounded-start">
+     
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Nombre Lugar</h5>
+        <h6>Ubicacion Lugar</h6>
+        <hr class="line";>
+        <p class="card-text"> El Teatro Nacional de Costa Rica es el principal teatro de Costa Rica.Se le considera uno de los inmuebles más 
+                importantes de la historia nacional, y principal joya arquitectónica de la ciudad de San José. Su construcción 
+                guarda un profundo significado dado que representa la decisión del costarricense a la hora de emprender acción y 
+                de la estabilidad económica (traída por la cosecha de café) y política de la época en la que fue construido.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="card mb-3" style="max-width: 1000px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Nombre Lugar</h5>
+        <h6>Ubicacion Lugar</h6>
+        <hr class="line";>
+        <p class="card-text"> El Teatro Nacional de Costa Rica es el principal teatro de Costa Rica.Se le considera uno de los inmuebles más 
+                importantes de la historia nacional, y principal joya arquitectónica de la ciudad de San José. Su construcción 
+                guarda un profundo significado dado que representa la decisión del costarricense a la hora de emprender acción y 
+                de la estabilidad económica (traída por la cosecha de café) y política de la época en la que fue construido.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3" style="max-width: 1000px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+    <img src="page/images/fototeatro.jpg" class="img-fluid rounded-start" alt="teatro nacional">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Nombre Lugar</h5>
+        <h6>Ubicacion Lugar</h6>
+        <hr class="line";>
+        <p class="card-text"> El Teatro Nacional de Costa Rica es el principal teatro de Costa Rica.Se le considera uno de los inmuebles más 
+                importantes de la historia nacional, y principal joya arquitectónica de la ciudad de San José. Su construcción 
+                guarda un profundo significado dado que representa la decisión del costarricense a la hora de emprender acción y 
+                de la estabilidad económica (traída por la cosecha de café) y política de la época en la que fue construido.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 
