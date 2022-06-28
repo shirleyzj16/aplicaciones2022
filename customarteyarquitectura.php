53d053755367501e622a2dd03a9d4558a5f4bbff
shirleyzj16 <shirleyzj16@gmail.com>
2022-06-28 14:25:32 -0600

    new

--- customarteyarquitectura.php
+++ customarteyarquitectura.php
@@ -3,16 +3,26 @@
 
 get_header(); ?>
 
+<?php
+      $args = array(
+                                'post_type'      => 'page',
+                                'posts_per_page' => -1,
+                                'post_parent'    => $post->ID,
+                                'orderby'          => 'rand'
+                            );
+                            $parent = new WP_Query( $args );
+
+                            if ( $parent->have_posts() ) : ?>
 
 	<?php while ( have_posts() ) : the_post(); ?>
 
-        <section class="artist-section py-4">
+        <section class="section py-4">
         <div class="container text-center">
             <div class="row">
                 <div class="col-12 px-2 px-lg-5">
                     <img class="img-fluid img-work" src="<?php the_field('imagen'); ?>" alt="Lugar">
                     <h2 class="pb-1"><?php the_title(); ?></h2>
-                    <p class="medio"><?php the_field('subtitulo'); ?></p>
+                    <p class="subtitulo"><?php the_field('subtitulo'); ?></p>
                     <p><?php the_content(); ?></p>
                     
                    
@@ -22,6 +32,8 @@ get_header(); ?>
     </section>
 
     <?php endwhile; ?>
+
+    <?php endif; wp_reset_postdata(); ?>
 
 </div>
 <?php get_footer(); ?>
