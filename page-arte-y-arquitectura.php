2a497c212ad75a0339da39660aee588eae3c578f
shirleyzj16 <shirleyzj16@gmail.com>
2022-07-12 08:59:02 -0600

    change

--- page-arte-y-arquitectura.php
+++ page-arte-y-arquitectura.php
@@ -1,5 +1,17 @@
 <?php get_header(); ?>
 
+<?php
+      $args = array(
+                                'post_type'      => 'page',
+                                'posts_per_page' => -1,
+                                'post_parent'    => $post->ID,
+                                'orderby'          => 'rand'
+                            );
+                            $parent = new WP_Query( $args );
+
+if ( $parent->have_posts() ) : ?>
+
+
 
 <?php
         //Loop
@@ -21,3 +33,5 @@
 
 
 <?php get_footer(); ?> 
+
+<?php endif; wp_reset_postdata(); ?>
